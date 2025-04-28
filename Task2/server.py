b16df049127f68d26f3f858b4ea3826155548bf6
Adnan Odeh <howare1971@gmail.com>
2025-04-28 22:29:42 +0300

    server side for task 2

--- Task2/server.py
+++ Task2/server.py
@@ -4,18 +4,57 @@
 
 import socket
 from config import HOST, PORT
+import os
 
 def run_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       #this line create TCP socket
-    server_socket.bind((HOST,PORT))     #bind -> sets the PORT number, and the HOST IP address 
-    server_socket.listen(2) #listen -> put the socket on listening mode, 1 -> referes to the maximum number of incoming connection requests
-    conn, add = server_socket.accept()
-    print(add)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(2)
+    
     while True:
+        conn, add = server_socket.accept()
+
         data = conn.recv(1024)
         
-        print(f"Data Received -> {data.decode()}")
-    conn.close()
-    server_socket.close()
+        with open("request.txt", "w") as file_to_write:
+            file_to_write.write(data.decode('utf-8'))
+        
+        print(f"Data Received -> {data.decode('utf-8')}")
+        
+        with open("request.txt", "r") as r:
+            for line in r:
+                if "filename" in line:
+                    input_value = line.split("=")
+                    
+                    if input_value[1].strip() == '':
+                        body = "<html><body><h1>Empty Input</h1></body></html>"
+                        response_empty = f"""HTTP/1.1 200 OK\r
+Content-Type: text/html\r
+Content-Length: {len(body.encode('utf-8'))}\r
+\r
+{body}"""
+                        conn.sendall(response_empty.encode('utf-8'))
+                        break
+                    
+                    else:
+                        file_path = os.path.join("media", input_value[1].strip())
+                        if os.path.isfile(file_path):
+                            body_img = f"""<html>
+                            <body>
+                            <h1>Computer Networks</h1>
+                            <img src="/mnt/c/Socket-Based-WebServer-Game/Socket-Based-WebServer-Game/Task2/media/net.jpeg" alt="Hello">
+                            </body>
+                            </html>"""
+
+                            response_file_exist = f"""HTTP/1.1 200 OK\r
+Content-Type: image/jpeg\r
+\r
+{open("media/net.jpeg", "rb").read()}"""
+
+                            conn.sendall(response_file_exist.encode('utf-8'))
+                            
+        conn.close()
+
+
     
 run_server()
